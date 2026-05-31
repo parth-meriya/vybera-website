3e4083472501edefbecb1d6a5d187c436e7cf4d9
parthmeriya <parthmeriya222@gmail.com>
2026-05-31 11:56:23 +0530

    fix: Support VITE_ prefixed env variables in Firebase Admin init

--- api/campaign/spin.js
+++ api/campaign/spin.js
@@ -5,9 +5,9 @@ function initAdmin() {
   if (!admin.apps.length) {
     admin.initializeApp({
       credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        projectId: process.env.FIREBASE_PROJECT_ID || process.env.VITE_FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL || process.env.VITE_FIREBASE_CLIENT_EMAIL,
+        privateKey: (process.env.FIREBASE_PRIVATE_KEY || process.env.VITE_FIREBASE_PRIVATE_KEY)?.replace(/\\n/g, '\n'),
       }),
     });
   }
